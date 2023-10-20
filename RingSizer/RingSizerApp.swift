2218784e3f1ab68fe963140596e9b8afa50f9463
Evgenii Mikhailov <e.mikhailov@outlook.com>
2023-10-20 15:12:16 +0300

    Initial Commit

---
+++ RingSizer/RingSizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RingSizerApp.swift
+//  RingSizer
+//
+//  Created by Evgenii Mikhailov on 20.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct RingSizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
